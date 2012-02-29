fed29073b993807e3799f9368f9dfc639a292329
Matt Atkins <matt@yoomee.com>
2012-02-29 17:41:37 +0000

    without returns all if given array of nil(s)

--- lib/ym_core/models/model.rb
+++ lib/ym_core/models/model.rb
@@ -3,7 +3,8 @@ module YmCore::Model
   def self.included(base)
     base.scope :without, (lambda do |item_or_array|
       if item_or_array.is_a?(Array)
-        base.where(["pages.id NOT IN (?)", item_or_array.collect(&:id).reject(&:nil?)])
+        array = item_or_array.collect{|i| i.try(:id)}.reject(&:nil?)
+        array.empty? ? base.scoped : base.where(["pages.id NOT IN (?)", array])
       else
         item_or_array.try(:id) ? base.where(["pages.id != ?", item_or_array.id]) : base.scoped
       end
