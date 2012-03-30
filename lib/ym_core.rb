3df3eaa2161c863f2b9556548393391a5dfe9e07
Ian Mooney <ian@yoomee.com>
2012-03-30 10:34:46 +0100

    Added require 'rails_rinku'

--- lib/ym_core.rb
+++ lib/ym_core.rb
@@ -2,11 +2,11 @@ require "ym_core/engine"
 require 'dragonfly'
 require 'will_paginate'
 require 'bootstrap-will_paginate'
+require 'rails_rinku'
 
 module YmCore
 end
 
-require 'rinku'
 require 'rails_config'
 
 require 'ym_core/models/model'
