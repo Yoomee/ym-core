75e40871837470012c57dd098a23a4979243d8d2
Matt Atkins <matt@yoomee.com>
2012-04-19 10:25:17 +0100

    Cache latest tweet. Set count to 10 when fetching latest tweet.

--- app/helpers/ym_core/twitter_helper.rb
+++ app/helpers/ym_core/twitter_helper.rb
@@ -15,21 +15,25 @@ module YmCore::TwitterHelper
 
   # TODO: cache latest_tweets
   def latest_tweets(screen_name, options = {})
-    options.reverse_merge!(:count => 1, :include_entities => false, :exclude_replies => true)
-    options[:screen_name] = screen_name
-    puts "http://api.twitter.com/1/statuses/user_timeline.json?#{options.to_param}"
-    tweets_json = open("http://api.twitter.com/1/statuses/user_timeline.json?#{options.to_param}").read
-    tweets_array = ActiveSupport::JSON.decode(tweets_json)
-    if tweets_array.is_a?(Array)
-      tweets_array.collect {|t| Struct::Tweet.new(t["text"], Time.parse(t["created_at"]), t["user"]["screen_name"], t["id"])}
-    else
-      Rails.logger.info("FAILED to fetch_latest_tweets_for #{screen_name}: Twitter error: #{tweets_array["error"]}")
-      []      
+    # NB exclude_replies is applied AFTER count  
+    options.reverse_merge!(:count => 1, :include_entities => false, :exclude_replies => true, :trim_user => true)
+  
+    Rails.cache.fetch("latest_tweets_#{screen_name}_#{options.to_query}", :expires_in => 10.minutes) do
+      options[:screen_name] = screen_name
+      puts "Fetching tweets"
+      tweets_json = open("http://api.twitter.com/1/statuses/user_timeline.json?#{options.to_param}").read
+      tweets_array = ActiveSupport::JSON.decode(tweets_json)
+      if tweets_array.is_a?(Array)
+        tweets_array.collect {|t| Struct::Tweet.new(t["text"], Time.parse(t["created_at"]), t["user"]["screen_name"], t["id"])}
+      else
+        Rails.logger.info("FAILED to fetch_latest_tweets_for #{screen_name}: Twitter error: #{tweets_array["error"]}")
+        []      
+      end
     end
   end
 
   def latest_tweet(screen_name, options = {})
-    latest_tweets(screen_name, options.merge(:count => 2)).first
+    latest_tweets(screen_name, options.merge(:count => 10)).first
   end
 
 end
