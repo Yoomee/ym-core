0bfa75839d7a7b7f6390745e51e58ee0a6026bed
Ian Mooney <ian@yoomee.com>
2012-06-26 09:41:57 +0100

    Add :after option onto date validator

--- app/validators/date_validator.rb
+++ app/validators/date_validator.rb
@@ -1,6 +1,11 @@
 class DateValidator < ActiveModel::EachValidator
   def validate_each(record,attribute,value)
-    record.errors[attribute] << "is invalid" unless value.blank? || value.to_s =~ UrlValidator::DATE_FORMAT
+    if options[:after]
+      message = options[:message] || "must be after #{options[:after]}"
+      record.errors[attribute] << message unless value.blank? || value > options[:after]
+    else
+      record.errors[attribute] << "is invalid" unless value.blank? || value.to_s =~ UrlValidator::DATE_FORMAT
+    end
   end
 end
 
