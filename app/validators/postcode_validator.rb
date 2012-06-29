b538f4bdd0c7ade4ffbcca6f86443051668db3cd
Edward Andrews <edward@yoomee.com>
2012-06-29 15:17:20 +0100

    Add a postcode validator

---
+++ app/validators/postcode_validator.rb
@@ -0,0 +1,7 @@
+class PostcodeValidator < ActiveModel::EachValidator
+  def validate_each(record,attribute,value)
+    record.errors[attribute] << "is not a valid postcode" unless value.to_s =~ PostcodeValidator::POSTCODE_FORMAT
+  end
+end
+
+PostcodeValidator::POSTCODE_FORMAT = /\A[A-Z]{1,2}[0-9R][0-9A-Z]? [0-9][A-Z-[CIKMOV]]{2}\z/
