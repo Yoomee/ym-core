55c9d60a43361a61e35019b856c5c5c7d133c4f6
Edward Andrews <edward@yoomee.com>
2012-07-31 16:31:20 +0100

    Remove decent_exposure 'page' from classes_for_body_tag

--- app/helpers/ym_core/layout_helper.rb
+++ app/helpers/ym_core/layout_helper.rb
@@ -60,8 +60,8 @@ module YmCore::LayoutHelper
       classes << "controller_#{controller_name}"
       classes << "action_#{action_name}"
       classes << (user_signed_in? ? 'logged_in' : 'logged_out')
-      classes << "root_slug_#{page.root_slug}" if defined?(page) && page.root && page.root_slug.present?
-      classes << "slug_#{page.slug}" if defined?(page) && page.slug.present?
+      classes << "root_slug_#{@page.root_slug}" if defined?(@page) && @page.root && @page.root_slug.present?
+      classes << "slug_#{@page.slug}" if defined?(@page) && @page.slug.present?
     end.join(' ')
   end
 
