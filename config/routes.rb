b7b768b35fd4d61a2b3fb8674231a2493940fbd7
Edward Andrews <edward@yoomee.com>
2012-08-01 10:58:18 +0100

    Move super to ym_users

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
   match "admin" => "admin#index", :as => :admin
-  match 'super' => 'super#index'
-  match 'super/morph' => 'super#morph', :via => :post
   resources :redactor_uploads, :only => [:index, :create]
 end
