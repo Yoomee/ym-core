10ae03bfac408e2e4e953eb005b9c52d2fc36ad2
Edward Andrews <edward@yoomee.com>
2012-11-12 11:26:55 +0000

    Add day_and_date date format

--- config/initializers/time_formats.rb
+++ config/initializers/time_formats.rb
@@ -1,7 +1,8 @@
 {
   :date => "%d/%m/%Y",
   :date_and_time => "%d/%m/%Y at %H:%M",
-  :month_day_year_or_not => "%B %o%O"
+  :month_day_year_or_not => "%B %o%O",
+  :day_and_date => "%a %e"
 }.each do |k,v|
   Time::DATE_FORMATS[k] = v
   Date::DATE_FORMATS[k] = v
