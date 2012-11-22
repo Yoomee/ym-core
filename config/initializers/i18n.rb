226d4c26d0fbc696d438d169bae6b6f127a36147
Matt Atkins <matt@yoomee.com>
2012-11-22 12:23:40 +0000

    Use english if translation is missing in production

---
+++ config/initializers/i18n.rb
@@ -0,0 +1,15 @@
+module I18n
+
+  def self.use_english_if_translation_missing(exception, locale, key, options)
+    if exception.is_a?(MissingTranslation) && locale != :en
+      I18n.translate(key, (options || {}).merge(:locale => :en))
+    elsif exception.is_a?(Exception)
+      raise exception
+    else
+      throw :exception, exception
+    end
+  end
+  
+end
+ 
+I18n.exception_handler = :use_english_if_translation_missing if Rails.env.production?
