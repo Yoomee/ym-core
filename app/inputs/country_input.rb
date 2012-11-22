de75d64f213ade66dbffac86b52990f5d918bf6e
Matt Atkins <matt@yoomee.com>
2012-11-22 12:55:14 +0000

    Make country_input work properly!

--- app/inputs/country_input.rb
+++ app/inputs/country_input.rb
@@ -4,9 +4,13 @@ class CountryInput < FormtasticBootstrap::Inputs::SelectInput
     @@countries ||= I18n.backend.send(:translations)[:en][:countries]
   end
   
+  def association_primary_key
+    "#{association_primary_key_for_method(method)}_code"
+  end
+  
   def select_html
-    countries = self.class.countries.map{|code,name| [I18n.translate("countries.#{code}"), code.to_s]}.sort
-    builder.select("#{input_name}_code", countries, input_options, input_html_options)
+    countries = CountryInput.countries.map{|code,name| [I18n.translate("countries.#{code}"), code.to_s]}.sort
+    builder.select(input_name, countries, input_options, input_html_options)
   end
   
 end
