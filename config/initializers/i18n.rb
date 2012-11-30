5359fff20add5e5f67a61101fb0aa8b58b041564
Andy Mayer <andy@yoomee.com>
2012-11-30 16:15:20 +0000

    Fix exception notifier for missing translation key and show user more useful info.

--- config/initializers/i18n.rb
+++ config/initializers/i18n.rb
@@ -2,8 +2,8 @@ module I18n
 
   def self.use_english_if_translation_missing(exception, locale, key, options)
     if exception.is_a?(MissingTranslation) && locale == :en
-      ExceptionNotifier::Notifier.exception_notification("production", StandardError.new, :data => {:message => "Missing key in config/locales/en.yml #{exception} #{key}"}).deliver
-      "English missing"
+      ExceptionNotifier::Notifier.exception_notification(Rails.env, StandardError.new("Missing key in config/locales/en.yml #{exception} #{key}")).deliver
+      key.split('.').last
     elsif exception.is_a?(MissingTranslation) && locale != :en
       I18n.translate(key, (options || {}).merge(:locale => :en))
     elsif exception.is_a?(Exception)
