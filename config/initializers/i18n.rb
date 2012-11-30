9ed8db8bf807e3c5060bb007d342c6305229393d
Edward Andrews <edward@aldreth.com>
2012-11-30 12:31:00 +0000

    Trying to fix translation error

--- config/initializers/i18n.rb
+++ config/initializers/i18n.rb
@@ -1,7 +1,10 @@
 module I18n
 
   def self.use_english_if_translation_missing(exception, locale, key, options)
-    if exception.is_a?(MissingTranslation) && locale != :en
+    if exception.is_a?(MissingTranslation) && locale == :en
+      ExceptionNotifier::Notifier.exception_notification("production", StandardError.new, :data => {:message => "Missing key in config/locales/en.yml #{exception} #{key}"}).deliver
+      "English missing"
+    elsif exception.is_a?(MissingTranslation) && locale != :en
       I18n.translate(key, (options || {}).merge(:locale => :en))
     elsif exception.is_a?(Exception)
       raise exception
