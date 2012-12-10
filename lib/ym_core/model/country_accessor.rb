ef4a0331402e870c267b97b803062b990e2e9821
Andy Mayer <andy@yoomee.com>
2012-12-10 16:42:55 +0000

    Fix bug so that translation ignores country codes that at nil

--- lib/ym_core/model/country_accessor.rb
+++ lib/ym_core/model/country_accessor.rb
@@ -10,7 +10,11 @@ module YmCore::Model::CountryAccessor
       attrs << :country if attrs.empty?
       attrs.each do |attr|
         define_method(attr) do
-          I18n.translate("countries.#{read_attribute("#{attr}_code")}")
+          if (attr_code = read_attribute("#{attr}_code")).present?
+            I18n.translate("countries.#{attr_code}")
+          else
+            nil
+          end
         end
         define_method("#{attr}_changed?") do
           send("#{attr}_code_changed?")
