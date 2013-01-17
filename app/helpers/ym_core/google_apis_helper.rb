3e65e8024a084b3d1037ededb50b82965082f3f6
Matt Atkins <matt@yoomee.com>
2013-01-17 14:00:22 +0000

    html_safe Google Analytics JS

--- app/helpers/ym_core/google_apis_helper.rb
+++ app/helpers/ym_core/google_apis_helper.rb
@@ -11,7 +11,7 @@ module YmCore::GoogleApisHelper
           var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
           ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
           var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-        })();"        
+        })();".html_safe
       end
     end
   end
