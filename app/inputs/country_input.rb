a3ba496b4e7477dd5523effdbf1b9cd4ae6afb1d
Matt Atkins <matt@yoomee.com>
2013-03-04 14:16:06 +0000

    Add priority_countries option to country input

--- app/inputs/country_input.rb
+++ app/inputs/country_input.rb
@@ -9,8 +9,14 @@ class CountryInput < FormtasticBootstrap::Inputs::SelectInput
   end
   
   def select_html
-    countries = CountryInput.countries.map{|code,name| [I18n.translate("countries.#{code}"), code.to_s]}.sort
-    builder.select(input_name, countries, input_options, input_html_options)
+    countries_array = []
+    country_codes = CountryInput.countries.sort_by(&:last).collect{|c| c.first.to_s}
+    if priority_country_codes = options.delete(:priority_country_codes)
+      countries_array += priority_country_codes.map{|code| [I18n.translate("countries.#{code}"), code.to_s]}
+      countries_array << ["-------------","  "]
+    end
+    countries_array += country_codes.map{|code| [I18n.translate("countries.#{code}"), code.to_s]}
+    builder.select(input_name, countries_array, input_options, input_html_options).sub(/>-------------/,"disabled=\"disabled\">-------------")
   end
   
 end
