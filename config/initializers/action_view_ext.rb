69e0640beea2079c1e234b7ac59623804269d5f1
Matt Atkins <matt@yoomee.com>
2013-04-17 14:11:48 +0100

    Add :icon_last option

--- config/initializers/action_view_ext.rb
+++ config/initializers/action_view_ext.rb
@@ -4,7 +4,11 @@ module ActionView
       def link_to_with_icon(*args, &block)
         html_options = args[2] || {}
         if !block_given? && (icon = html_options.delete(:icon))
-          args[0] = "<i class='icon-#{icon}'></i> #{args[0]}".strip.html_safe
+          if html_options.delete(:icon_last)
+            args[0] = "#{args[0]} <i class='icon-#{icon}'></i>".strip.html_safe
+          else
+            args[0] = "<i class='icon-#{icon}'></i> #{args[0]}".strip.html_safe
+          end
           args[2] = html_options
         end
         link_to_without_icon(*args, &block).html_safe
