947a18e721fced9b0cfe60c9410513573633f3aa
Ian Mooney <ian@yoomee.com>
2013-05-28 10:43:58 +0100

    Update google_analytics code to use universal analytics

--- app/helpers/ym_core/google_apis_helper.rb
+++ app/helpers/ym_core/google_apis_helper.rb
@@ -2,17 +2,28 @@ module YmCore::GoogleApisHelper
   
   def google_analytics_js(options = {})
     if !(Rails.env =~ /development|test/) && (tracker_code = Settings.google_analytics).present?
+      options.reverse_merge!(:universal => true, :domain => Settings.site_url.sub(/(^https?:\/\/(www\.)?)?/, ''))
       javascript_tag do
-        "var _gaq = _gaq || [];
-        _gaq.push(['_setAccount', '#{tracker_code}']);
-        #{"_gaq.push(['_setDomainName', '#{options[:domain]}']);" if options[:domain]}
-        _gaq.push(['_trackPageview']);
+        if options[:universal]
+          "(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+          (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+          m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+          })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-        (function() {
-          var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-          ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-          var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-        })();".html_safe
+          ga('create', '#{tracker_code}', '#{options[:domain]}');
+          ga('send', 'pageview');".html_safe
+        else
+          "var _gaq = _gaq || [];
+          _gaq.push(['_setAccount', '#{tracker_code}']);
+          #{"_gaq.push(['_setDomainName', '#{options[:domain]}']);" if options[:domain]}
+          _gaq.push(['_trackPageview']);
+
+          (function() {
+            var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+            ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+            var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+          })();".html_safe
+        end
       end
     end
   end
