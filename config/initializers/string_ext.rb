7b18473c7ab39b59c1579c5a55637303fb20d76a
Ian Mooney <ian@yoomee.com>
2013-06-05 17:28:01 +0100

    Add valid_email? to string extensions

--- config/initializers/string_ext.rb
+++ config/initializers/string_ext.rb
@@ -1,5 +1,13 @@
 class String
 
+  def valid_email?
+    if self =~ /\A[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]+\z/
+      true
+    else
+      false
+    end
+  end
+
   def is_number?
     true if Float(self) rescue false
   end
