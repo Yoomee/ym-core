5ea651907bc7282657f8d2db5f7e1b0d53c9d3e9
Ian Mooney <ian@yoomee.com>
2013-11-08 13:43:53 +0000

    Add indefinitize method to String

--- config/initializers/string_ext.rb
+++ config/initializers/string_ext.rb
@@ -8,6 +8,10 @@ class String
     end
   end
 
+  def indefinitize
+    (%w{a e i o u}.include?(self.first.downcase) ? "an " : "a ") + self
+  end
+
   def is_number?
     true if Float(self) rescue false
   end
