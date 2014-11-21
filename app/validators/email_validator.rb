ad5e66cd52bbe507aea0a49e56d712b92e6ac607
Greg Woodcock <greg@yoomee.com>
2014-11-21 10:57:26 +0000

    Update the email validator as the current on no longer works with the latest version of the mail gem

--- app/validators/email_validator.rb
+++ app/validators/email_validator.rb
@@ -1,21 +1,16 @@
-require 'mail'
+# Based on work from http://thelucid.com/2010/01/08/sexy-validation-in-edge-rails-rails-3/
 class EmailValidator < ActiveModel::EachValidator
-  def validate_each(record,attribute,value)
-    begin
-      m = Mail::Address.new(value)
-      # We must check that value contains a domain and that value is an email address
-      r = m.domain && m.address == value
-      t = m.__send__(:tree)
-      # We need to dig into treetop
-      # A valid domain must have dot_atom_text elements size > 1
-      # user@localhost is excluded
-      # treetop must respond to domain
-      # We exclude valid email values like <user@localhost.com>
-      # Hence we use m.__send__(tree).domain
-      r &&= (t.domain.dot_atom_text.elements.size > 1)
-    rescue Exception => e   
-      r = false
+  @@default_options = {}
+
+  def self.default_options
+    @@default_options
+  end
+
+  def validate_each(record, attribute, value)
+    options = @@default_options.merge(self.options)
+    name_validation = options[:strict_mode] ? "-a-z0-9+._" : "^@\\s"
+    unless value =~ /\A\s*([#{name_validation}]{1,64})@((?:[-a-z0-9]+\.)+[a-z]{2,})\s*\z/i
+      record.errors.add(attribute, options[:message] || :invalid)
     end
-    record.errors[attribute] << (options[:message] || "is invalid") unless r
   end
 end
