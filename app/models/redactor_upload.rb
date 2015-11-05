1d4ec74c554ea6390eb5a5d722b060daed4f6fe6
Greg Woodcock <greg@yoomee.com>
2015-11-05 11:23:04 -0500

    Update validate -> validates for rails 4

--- app/models/redactor_upload.rb
+++ app/models/redactor_upload.rb
@@ -2,7 +2,7 @@ class RedactorUpload < ActiveRecord::Base
   
   image_accessor :file
   validates_property :format, :of => :image, :in => [:jpeg, :jpg, :png, :gif], :message => "must be an image"
-  validate :file_uid, :presence => true
+  validates :file_uid, :presence => true
   
   def image
     file_type == 'image' ? file : nil
