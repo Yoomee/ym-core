b576723b6161e10ec3cd0164af8d0f3744390dd2
Tom Kitchin <tom@yoomee.com>
2015-11-09 11:12:13 -0500

    Remove erroneous validation

--- app/models/redactor_upload.rb
+++ app/models/redactor_upload.rb
@@ -2,7 +2,6 @@ class RedactorUpload < ActiveRecord::Base
   
   image_accessor :file
   validates_property :format, :of => :image, :in => [:jpeg, :jpg, :png, :gif], :message => "must be an image"
-  validates :file_uid, :presence => true
   
   def image
     file_type == 'image' ? file : nil
